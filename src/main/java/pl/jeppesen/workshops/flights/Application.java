2970e3b75bb424ff408588da92500e742d45300e
Mariusz Strzelecki <szczeles@gmail.com>
2016-10-30 18:31:46 +0100

    usage of range validator in application

--- src/main/java/pl/jeppesen/workshops/flights/Application.java
+++ src/main/java/pl/jeppesen/workshops/flights/Application.java
@@ -2,6 +2,7 @@ package pl.jeppesen.workshops.flights;
 
 import pl.jeppesen.workshops.flights.dataprovider.CsvFlightDataProvider;
 import pl.jeppesen.workshops.flights.model.Flight;
+import pl.jeppesen.workshops.flights.validator.DateRangeFlightValidator;
 import pl.jeppesen.workshops.flights.validator.FlightValidator;
 import pl.jeppesen.workshops.flights.validator.ValidIdFlightValidator;
 
@@ -12,6 +13,7 @@ import java.util.function.Predicate;
 public class Application {
     public static void main(String[] args) {
         FlightValidator flightValidator = new ValidIdFlightValidator();
+        DateRangeFlightValidator dateRangeFlightValidator = new DateRangeFlightValidator("2015-10-01", "2016-10-01");
         CsvFlightDataProvider flightDataProvider = new CsvFlightDataProvider("data/flights.csv");
 
 
@@ -19,6 +21,7 @@ public class Application {
         System.out.println(flights.size());
         System.out.println(flights.stream()
                 .filter(flight -> flightValidator.isValid(flight))
-                .filter(flight -> flight.getDate().isBefore(LocalDate.of(2015, 11, 1))).count());
+                .filter(flight -> dateRangeFlightValidator.isValid(flight))
+                .count());
     }
 }
