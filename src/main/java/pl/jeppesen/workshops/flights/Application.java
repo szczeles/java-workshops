284ee642fea10f6dcc055fb386c50fe1bb75ff58
Mariusz Strzelecki <szczeles@gmail.com>
2016-10-30 17:44:27 +0100

    counting valid flights

--- src/main/java/pl/jeppesen/workshops/flights/Application.java
+++ src/main/java/pl/jeppesen/workshops/flights/Application.java
@@ -6,13 +6,22 @@ import pl.jeppesen.workshops.flights.validator.FlightValidator;
 import pl.jeppesen.workshops.flights.validator.ValidIdFlightValidator;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         FlightValidator flightValidator = new ValidIdFlightValidator();
         CsvFlightDataProvider flightDataProvider = new CsvFlightDataProvider("data/flights.csv");
-        System.out.println(flightDataProvider.getFlights().size());
 
-        //System.out.println(flightValidator.isValid(flight));
+
+        List<Flight> flights = flightDataProvider.getFlights();
+        System.out.println(flights.size());
+        int valid = 0;
+        for (Flight flight : flights) {
+            if (flightValidator.isValid(flight)) {
+                valid++;
+            }
+        }
+        System.out.println(valid);
     }
 }
