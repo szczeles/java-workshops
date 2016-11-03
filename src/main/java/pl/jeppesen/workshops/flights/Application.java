902369ee3b959ecfa1b9e9c8988c1a09310ed9f0
Mariusz Strzelecki <szczeles@gmail.com>
2016-11-03 23:08:50 +0100

    executor framework

--- src/main/java/pl/jeppesen/workshops/flights/Application.java
+++ src/main/java/pl/jeppesen/workshops/flights/Application.java
@@ -9,11 +9,16 @@ import pl.jeppesen.workshops.flights.flight.Flight;
 import pl.jeppesen.workshops.flights.flight.dataprovider.CsvFlightDataProvider;
 import pl.jeppesen.workshops.flights.flight.validator.*;
 
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class Application {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         Configuration configuration = new Configuration("src/main/resources/configuration.xml");
 
         AircraftDataProvider aircraftDataProvider = new SqliteAircraftDataProvider("data/aircrafts.db");
@@ -21,19 +26,32 @@ public class Application {
         FlightValidator flightValidator = new OverallFlightValidator(Lists.newArrayList(
                 new ValidIdFlightValidator(),
                 new DateRangeFlightValidator(configuration.getDateRangeFrom(), configuration.getDateRangeTo()),
-                new AirportFlightValidator(configuration.getAirports()),
-                new ValidAircraftFlightValidator(aircraftDataProvider)
+                new AirportFlightValidator(configuration.getAirports())
         ));
+//
+//        ,
+//        new ValidAircraftFlightValidator(aircraftDataProvider)
 
         CsvFlightDataProvider flightDataProvider = new CsvFlightDataProvider(configuration.getFlightsCsvPath());
 
-        //Stopwatch stopwatch = Stopwatch.createStarted();
-        Stream<Flight> stream = StreamSupport.stream(flightDataProvider.getFlightsIterator().spliterator(), false)
-                .filter(flight -> flightValidator.isValid(flight));
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        //Stream<Flight> stream = StreamSupport.stream(flightDataProvider.getFlightsIterator().spliterator(), false)
+        //        .filter(flight -> flightValidator.isValid(flight));
 
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        ArrayList<Future<Boolean>> tasks = Lists.newArrayList();
+        for (Flight f : flightDataProvider.getFlightsIterator()) {
+            tasks.add(executorService.submit(() -> flightValidator.isValid(f)));
+        }
+        int valid = 0;
+        for (Future<Boolean> task : tasks) {
+            if (task.get()) {
+                valid ++;
+            }
+        }
+        System.out.println(valid);
 
-
-        //System.out.println(stopwatch);
+        System.out.println(stopwatch);
 
     }
 
